46f6403f3af3c0e63d273bba3bc81c3f45fa1132
baochuquan <baochuquan@163.com>
2024-04-24 19:00:37 +0800

    feat: 优化逻辑

--- lib/gitreviewer/command.rb
+++ lib/gitreviewer/command.rb
@@ -9,7 +9,7 @@ module GitReviewer
         self.abstract_command = false
 
         self.description = <<-DESC
-            git-reviewer is a git plugin used to analyze who should review a Merge Request or Pull Request, and more details related to code mofitifications.
+            git-reviewer is a git plugin used to analyze who should review a Merge Request or Pull Request, and more details related to code modifications.
         DESC
 
         self.command = 'git-reviewer'
@@ -30,28 +30,50 @@ module GitReviewer
             @init = argv.flag?('init', false)
             @target = argv.option('target')
             @source = argv.option('source')
-            @analyze_reviewer = argv.flag?('reviewer', true)
-            @analyze_author = argv.flag?('author', true)
+            @analyze_reviewer = argv.flag?('reviewer', false)
+            @analyze_author = argv.flag?('author', false)
             @verbose = argv.flag?('verbose', false)
             @version = argv.flag?('version', false)
+            @help = argv.flag?('help', false)
             super
         end
 
-        def validate!
-            super
-            if !@init && (@analyze_author || @analyze_reviewer)
-                help! "Please execute the `git reviewer --init` command first to initialize the code review configuration file of the Git repository."
-            end
-        end
+        # def validate!
+        #     super
+        #     # 处理 help 选项
+        #     if @help
+        #         help!
+        #         return
+        #     end
+        # end
 
         def run
+            # 处理 help 选项
+              if @help
+                help!
+                return
+              end
+
+            # 处理 version 选项
             if @version
                 puts "git-reviewer #{GitReviewer::VERSION}"
                 return
             end
-            # analyzer = Analyzer.new("feature/changeEnLevel", "master")
-            # analyzer.setupReviewers
-            # analyzer.makeDecision
+
+            # 处理 init 选项
+            if @init
+                puts "init"
+                return
+            end
+
+            # 分析
+            if !@analyze_author && !@analyze_reviewer
+                # 如果两个选项均没有，则默认分析作者和审查者
+                @analyze_author = true
+                @analyze_reviewer = true
+
+                puts "analyze"
+            end
         end
     end
 end
